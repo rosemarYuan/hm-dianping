bf33df4bbdcac30627f25437fd767c15b0e7d169
keruiWang <wangkerui@mac>
2025-12-19 11:13:32 +0800

    -发送微信验证码 删Todo

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -36,7 +36,7 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
+        log.info("发送短信验证码并保存验证码, phone: {}", phone);
         return userInfoService.sentCode(phone, session);
     }
 
