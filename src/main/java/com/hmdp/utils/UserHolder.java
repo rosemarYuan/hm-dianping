de0be4ebfbce3b1e5d5fa0aabdfdbc5f5773fd8e
keruiWang <wangkerui@mac>
2025-12-21 13:35:27 +0800

    -登陆校验 拦截器 隐藏敏感信息(DTO)

--- src/main/java/com/hmdp/utils/UserHolder.java
+++ src/main/java/com/hmdp/utils/UserHolder.java
@@ -2,8 +2,9 @@ package com.hmdp.utils;
 
 import com.hmdp.dto.UserDTO;
 
-public class UserHolder {
+public class  UserHolder {
     private static final ThreadLocal<UserDTO> tl = new ThreadLocal<>();
+
 
     public static void saveUser(UserDTO user){
         tl.set(user);
