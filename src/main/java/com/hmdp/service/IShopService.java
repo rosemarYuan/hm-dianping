935bb9dbc95e90abf3eb6c6a6cc79466c42d216e
keruiWang <wangkerui@mac>
2025-12-23 17:28:29 +0800

    - 实现商铺缓存

--- src/main/java/com/hmdp/service/IShopService.java
+++ src/main/java/com/hmdp/service/IShopService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 
 /**
@@ -13,5 +14,5 @@ import com.hmdp.entity.Shop;
  */
 public interface IShopService extends IService<Shop> {
 
-    void queryShopById(Long id);
+    Result queryShopById(Long id);
 }
--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -15,18 +22,38 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
-    @Override
-    public void queryShopById(Long id) {
-        //  TODO 数据缓存实现
-        //   1 Redis查询商铺缓存
-        //   2 判断缓存是否命中
-        //   (否)  3 未命中时，使用MBP根据id查询数据库
-        //         4 判断商铺在数据库是否存在
-        //              (否) 返回404
-        //              (是) 商铺数据写入Redis
-        //   5 返回商铺信息
+    private final StringRedisTemplate stringRedisTemplate;
 
+    public ShopServiceImpl(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public Result queryShopById(Long id) {
+        String key = CACHE_SHOP_KEY + id;
+        //   1 Redis查询商铺缓存
+        String shopJsonCache = stringRedisTemplate.opsForValue().get(key);
+        //   2 判断缓存是否命中，命中直接返回
+        if (StringUtils.isNotBlank(shopJsonCache)) {
+            Shop shop = JSONUtil.toBean(shopJsonCache, Shop.class);
+            return Result.ok(shop);
+        }
+
+        //   3 未命中时，使用MBP根据id查询数据库
+        Shop shop = getById(id);
+        //   4 判断商铺在数据库是否存在
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+
+        //   5 商铺数据写入Redis
+        String jsonShop = JSONUtil.toJsonStr(shop);
+        stringRedisTemplate.opsForValue().set(key, jsonShop);
+        log.debug("已经完成向{}缓存数据：{}",key,jsonShop);
+        //   6 返回商铺信息
+        return Result.ok(shop);
     }
 }
