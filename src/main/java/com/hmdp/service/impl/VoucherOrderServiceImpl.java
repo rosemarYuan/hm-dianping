b2df95f09a219c045e3804d95014281a9f436d89
keruiWang <wangkerui@mac>
2025-12-30 20:05:55 +0800

    优惠券秒杀： 全局id生成器

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +26,55 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService seckillVoucherService;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        // 1 查询优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+
+        // 2 判断秒杀是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
+            // 未开始
+            return Result.fail("秒杀尚未开始");
+        }
+
+        // 3 判断秒杀是否结束
+        if(voucher.getEndTime().isBefore(LocalDateTime.now())) {
+            return Result.fail("秒杀已经结束");
+        }
+
+        // 4 判断库存是否充足
+        if(voucher.getStock()<=0){
+            return Result.fail("已经售空");
+        }
+
+        // 5 扣减库存
+        boolean success = seckillVoucherService.update()
+                .setSql("stock = stock -1")
+                .eq("voucher_id", voucherId)
+                //.eq("stock", voucher.getStock()) // 乐观锁接口，体现缺点是同时所有接口只有一个可以成功，即成功率太低
+                .gt("stock", 0)
+                .update();
+        if(!success){
+            // 付款失败
+            return Result.fail("扣减失败");
+        }
+
+        // 6 创建订单,需要手填订单id、用户id、代金券id
+        VoucherOrder voucherOrder = new VoucherOrder();
+        voucherOrder.setVoucherId(voucherId);
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        voucherOrder.setId(redisIdWorker.nextId("order"));
+        // 订单写入数据库
+        save(voucherOrder);
+
+        // 7 返回订单id
+        return Result.ok(redisIdWorker.nextId("order"));
+    }
 }
